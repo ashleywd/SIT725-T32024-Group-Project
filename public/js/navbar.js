2ad1a0f4a3276e3017cf3a91b9084e31ffeb9104
marlon-deakin <s218516527@deakin.edu.au>
2024-12-18 17:54:24 +1100

    fixed a typo in the navbar for the account menu, added the required code for acct. menu

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -2,12 +2,14 @@ const loginMenu = document.querySelector("#login-menu");
 const registerMenu = document.querySelector("#register-menu");
 const logoutMenu = document.querySelector("#logout-menu");
 const dashboardMenu = document.querySelector("#dashboard-menu");
+const accountMenu = document.querySelector("#account-menu")
 
 const toggleUnnecessaryMenu = () => {
   loginMenu.style.display = "none";
   registerMenu.style.display = "none";
   dashboardMenu.style.display = "block";
   logoutMenu.style.display = "block";
+  accountMenu.style.display = "block";
 };
 
 const checkAuth = () => {
