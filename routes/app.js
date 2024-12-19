0140c4b076f6c5ed10cac0a22ba22003a9487a93
marlon-deakin <s218516527@deakin.edu.au>
2024-12-19 14:19:34 +1100

    commiting after removing a routing that was causing an issue

--- routes/app.js
+++ routes/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const appController = require('../controllers/appController');
-
 // Basic route handlers
 router.get('/', appController.renderHome);
 router.get('/dashboard', appController.renderDashboard);
 router.get('/login', appController.renderLogin);
 router.get('/register', appController.renderRegister);
-router.get('/account',appController.renderAccount);
+router.get('/account', appController.renderAccount); // Serve the EJS view
 
 module.exports = router;
