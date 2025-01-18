2ea447e2b442e2e7611e138e6e746610158aebf1
marlon-deakin <s218516527@deakin.edu.au>
2025-01-18 20:29:42 +1100

    changed the 'load the login page' by using the elements id

--- test/e2e/login.test.js
+++ test/e2e/login.test.js
@@ -10,10 +10,10 @@ test.describe('Login Functionality', () => {
   // Test if the login page loads successfully
   test('should load the login page', async ({ page }) => {
     const title = await page.title();
-    expect(title).toBe(''); 
+    expect(title).toBe('Login'); 
     
-    await expect(page.locator('input[name="username"]')).toBeVisible();
-    await expect(page.locator('input[name="password"]')).toBeVisible();
+    await expect(page.locator('#username')).toBeVisible();
+    await expect(page.locator('#password')).toBeVisible();
     await expect(page.locator('button[type="submit"]')).toBeVisible();
   });
 
