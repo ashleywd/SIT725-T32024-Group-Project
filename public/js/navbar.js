c650dd99be73222aa34d98774f362615ab6d536d
ashleywd <ashleywd@live.com.au>
2025-01-18 11:25:04 +1100

    Resolve issue with points display showing at login

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -5,15 +5,17 @@ const dashboardMenu = document.querySelectorAll("#dashboard-menu");
 const accountMenu = document.querySelectorAll("#account-menu");
 const myPostsMenu = document.querySelectorAll("#my-posts-menu");
 const notificationsMenu = document.querySelectorAll("#notifications-menu");
+const pointsDisplay = document.querySelectorAll("#pointsBadge");
 
 const toggleUnnecessaryMenu = () => {
-  loginMenu.forEach((menu) => menu.style.display = "none")
-  registerMenu.forEach((menu) => menu.style.display = "none");
-  dashboardMenu.forEach((menu) => menu.style.display = "block");
-  myPostsMenu.forEach((menu) => menu.style.display = "block");
-  logoutMenu.forEach((menu) => menu.style.display = "block");
-  accountMenu.forEach((menu) => menu.style.display = "block");
-  notificationsMenu.forEach((menu) => menu.style.display = "block");
+  loginMenu.forEach((menu) => (menu.style.display = "none"));
+  registerMenu.forEach((menu) => (menu.style.display = "none"));
+  dashboardMenu.forEach((menu) => (menu.style.display = "block"));
+  myPostsMenu.forEach((menu) => (menu.style.display = "block"));
+  logoutMenu.forEach((menu) => (menu.style.display = "block"));
+  accountMenu.forEach((menu) => (menu.style.display = "block"));
+  notificationsMenu.forEach((menu) => (menu.style.display = "block"));
+  pointsDisplay.forEach((menu) => (menu.style.display = "block"));
 };
 
 const logout = (e) => {
@@ -24,6 +26,4 @@ const logout = (e) => {
 
 logoutMenu.forEach((menu) => menu.addEventListener("click", logout));
 
-export {
-  toggleUnnecessaryMenu,
-};
+export { toggleUnnecessaryMenu };
