cc8c8fe8861935217c815ac877729d6f33fda32d
ashleywd <ashleywd@live.com.au>
2025-01-19 09:31:33 +1100

    Removed unnessasary reference to User model

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,3 @@
-const User = require("../models/user");
 const Post = require("../models/post");
 const Notification = require("../models/notification");
 const pointsController = require("./pointsController");
