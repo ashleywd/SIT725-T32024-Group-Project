2710bf009ebf017580b8c9282fa70da8bc37fa69
daosgava <daosgava.garcia@gmail.com>
2025-01-28 21:28:27 +1100

    remove additional comments

--- playwright.config.js
+++ playwright.config.js
@@ -2,11 +2,6 @@
 import { defineConfig, devices } from '@playwright/test';
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-
-/**
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
